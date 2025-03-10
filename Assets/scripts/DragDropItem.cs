6e18af20c32acd0d813611509c343e66bfe23249
SHP256 <114565@falbk.schule>
2025-03-10 12:38:23 +0100

    DragAndDrop Logik

--- Assets/scripts/DragDropItem.cs
+++ Assets/scripts/DragDropItem.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragDropItem : MonoBehaviour
+public class DragDropItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
     {
-        
+        canvas = GetComponentInParent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        canvasGroup.alpha = 0.6f; 
+        canvasGroup.blocksRaycasts = false; 
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f; 
+        canvasGroup.blocksRaycasts = true; 
     }
 }
+
