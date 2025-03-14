47b12aca47ec8bbb05a2be475192c849006b57de
Simon Hetzer (MacBook) <simon.hetzer@icloud.com>
2025-03-14 21:10:31 +0100

    Transition MainMenu & Credits

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -9,25 +9,26 @@ public class MainMenu : MonoBehaviour
     public Button creditsButton;
     public Button exitButton;
 
+    public GameObject creditsPanel, gameStatePanel;
 
     void Start()
     {
-        // Start Button Position und Größe
+        // Start Button Position und Grï¿½ï¿½e
         RectTransform startRect = startButton.GetComponent<RectTransform>();
         startRect.anchoredPosition = new Vector2(0, 200);
         startRect.sizeDelta = new Vector2(600, 150);
 
-        // Sound Button Position und Größe
+        // Sound Button Position und Grï¿½ï¿½e
         RectTransform soundRect = soundButton.GetComponent<RectTransform>();
         soundRect.anchoredPosition = new Vector2(0, 0);
         soundRect.sizeDelta = new Vector2(600, 150);
 
-        // Credits Button Position und Größe
+        // Credits Button Position und Grï¿½ï¿½e
         RectTransform creditsRect = creditsButton.GetComponent<RectTransform>();
         creditsRect.anchoredPosition = new Vector2(0, -200);
         creditsRect.sizeDelta = new Vector2(600, 150);
 
-        // Exit Button Position und Größe
+        // Exit Button Position und Grï¿½ï¿½e
         RectTransform exitRect = exitButton.GetComponent<RectTransform>();
         exitRect.anchoredPosition = new Vector2(0, -400);
         exitRect.sizeDelta = new Vector2(600, 150);
@@ -43,8 +44,10 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        // Hier die Szene laden, die das Spiel startet
-        SceneManager.LoadScene("MainScene");
+        SoundEffectsManager.SFX.PlayButtonSound();
+        FindFirstObjectByType<EventManager>(FindObjectsInactive.Include).gameObject.SetActive(true);
+        gameStatePanel.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     public void ToggleSound()
@@ -54,8 +57,9 @@ public class MainMenu : MonoBehaviour
     }
     public void ShowCredits()
     {
-        // Hier die Szene laden, die die Credits anzeigt
-        SceneManager.LoadScene("CreditsScene");
+        SoundEffectsManager.SFX.PlayButtonSound();
+        creditsPanel.SetActive(true);
+        gameObject.SetActive(false);
     }
     public void ExitGame()
     {
